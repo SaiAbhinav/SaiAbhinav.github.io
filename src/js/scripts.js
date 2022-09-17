1fd8e953e190e4ef42527925dcb9b9d1cd28cc7f
SaiAbhinav <saiabhinavvasepalli@gmail.com>
2022-09-17 00:19:23 -0400

    [v0.1.0] Updated nav and added contact section

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,6 +1,7 @@
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 const brand = document.querySelector(".mobile-menu-brand");
+const backToTop = document.querySelector("#back-to-top");
 
 btn.addEventListener("click", () => {
   menu.classList.toggle("hidden");
@@ -10,17 +11,24 @@ const nav = document.querySelector("nav");
 
 function afterScroll() {
   if (
-    document.body.scrollTop > 100 ||
-    document.documentElement.scrollTop > 100
+    document.body.scrollTop > 50 ||
+    document.documentElement.scrollTop > 50 
   ) {
-    nav.classList.add("shadow-lg");
-    nav.querySelector(".nav-container").classList.add("py-0");
+    nav.classList.add("shadow-lg", "bg-white");
+    nav.querySelector(".nav-container").classList.add("py-2");
     brand.classList.add("py-4");
+    backToTop.style.display = "flex";
   } else {
-    nav.classList.remove("shadow-lg");
-    nav.querySelector(".nav-container").classList.remove("py-0");
+    nav.classList.remove("shadow-lg", "bg-white");
+    nav.querySelector(".nav-container").classList.remove("py-2");
     brand.classList.remove("py-4");
+    backToTop.style.display = "none";
   }
+}
+
+function toTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
 
 window.addEventListener("load", afterScroll);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        'section': '82rem'
+      },
+      minHeight: {
+        'section': 'calc(100vh - 7rem)'
+      },
+      scrollMargin: {
+        '22': '5.5rem'
+      }
+    },
   },
   plugins: [],
 }
